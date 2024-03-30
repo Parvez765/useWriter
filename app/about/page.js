54bfe14b3496f1d24e587b589c4f8b4e712aa8cf
ParvezHossainSakib <pervezhossain1998@gmail.com>
2024-03-30 14:14:27 +0600

    Folder Structure Organized

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout/Layout";
+const about = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Hello this is about</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default about;
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout/Layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+        <h1>Hello World</h1>
+      </Layout>
+    </>
+  );
+}
